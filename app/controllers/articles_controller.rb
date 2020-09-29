8f425dbf2d440ca8f371ffd3ffec39e2e7c59046
Muhammed Cihad Kurel <65771498+mcihadkurel@users.noreply.github.com>
2020-09-29 14:11:56 +0300

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,21 @@
-class ArticlesController < ApplicationController 
-    #Controller page
+class ArticlesController < ApplicationController
+    
+    def index
+        @articles = Article.all
+    end
 
+    def show
+        @article = Article.find(params[:id])
+    end
     def new
         @article = Article.new
-      end       
-      def create
-        @article = Article.new(article_params)
-       
-        if @article.save
-          redirect_to @article
-        else
-          render 'new'
-        end
+    end
+    def create
+        @article = Article.new(
+            title: params[:article][:title],
+            body: params[:article][:body])
+          @article.save
+          redirect_to article_path(@article)
       end
-  end
+
+end
