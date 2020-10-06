b9675af6e8d3957162c7d6ba0ffdc0d5626801f5
Muhammed Cihad Kurel <65771498+mcihadkurel@users.noreply.github.com>
2020-10-06 08:25:50 +0300

    Add foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -41,5 +41,9 @@ ActiveRecord::Schema.define(version: 2020_09_29_092432) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
+  
+  add_foreign_key 'comments', 'articles'
+  add_foreign_key 'taggings', 'articles'
+  add_foreign_key 'taggings', 'tags'
 
 end
